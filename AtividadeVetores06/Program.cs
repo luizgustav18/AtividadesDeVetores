aecd31a8e8e899c5009da383fc7b22deaad60bb6
Luiz Gustavo <luizgustavoms18@gmail.com>
2026-03-25 20:00:50 -0300

    commit

---
+++ AtividadeVetores06/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double[] vetor = new double[10];
+
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            Console.Write("Digite o salário do funcionario " + (i+1) + " : " + "R$");
+            vetor[i] = double.Parse(Console.ReadLine());
+
+            vetor[i] = vetor[i] + (vetor[i] * 0.05);
+        }
+
+        Console.WriteLine("\nSalários reajustados:\n");
+
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            Console.WriteLine("Funcionario " + (i + 1) + ": " + vetor[i].ToString("F2"));
+        }
+    }
+}
---
+++ AtividadesVetores05/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.Serialization;
+class Program
+{
+    static void Main()
+    {
+        int impares = 0;
+        int pares = 0;
+
+        Console.Write("Digite valores para o vetor: ");
+        int[] vetor = new int[5];
+
+        int maior = 0;
+        int menor = 0;
+
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            vetor[i] = int.Parse(Console.ReadLine());
+            if (vetor[i] % 2 != 0)
+            {
+                impares++;
+            }
+            else
+            {
+                pares++;
+            }
+            if (maior > 50)
+            {
+                maior++;
+            }
+            else if (menor < 7)
+            {
+                menor++;
+            }
+        }
+        Console.WriteLine("Números impares do vetor: " + impares);
+        Console.WriteLine("Números pares do vetor: " + pares);
+        Console.WriteLine("Números maoiores que 50 do vetor: " + maior);
+        Console.WriteLine("Números menores que 7 do vetor: " + menor);
+    }
+}
