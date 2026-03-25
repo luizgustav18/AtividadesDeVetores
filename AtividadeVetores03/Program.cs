7cf3290a16bacdfbe5a8c34d85396d0723b4f667
Luiz Gustavo <luizgustavoms18@gmail.com>
2026-03-25 19:41:53 -0300

    commit

---
+++ AtividadeVetores03/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite os valores do vetor: ");
+        int[] vetor = new int[5];
+
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            vetor[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.Clear();
+
+        Console.WriteLine("Digite um valor para ver se está no vetor: ");
+        int valor = int.Parse(Console.ReadLine());
+
+        int contador = 0;
+
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            if (vetor[i] == valor)
+            {
+                Console.WriteLine("O " + valor + " foi encontrado na posição: " + i);
+                contador++;
+            }
+        }
+
+        if (contador == 0)
+        {
+            Console.WriteLine("Não tem esse número no vetor!");
+        }
+    }
+}
---
+++ AtividadeVetores04/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite um valores para preencher o vetor 1: ");
+        int[] vetor1 = new int[10];
+        for (int i = 0; i < 10; i++)
+        {
+            vetor1[i] = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("Digite um valores para preencher o vetor 2: ");
+        int[] vetor2 = new int[10];
+        for (int i = 0; i < 10; i++)
+        {
+            vetor2[i] = int.Parse(Console.ReadLine());
+        }
+
+        int[] vetor3 = new int[10];
+
+        for (int i = 0; i< 10; i++)
+        {
+            vetor3[i] = vetor1[i] + vetor2[i];
+        }
+        Console.WriteLine("A soma dos vetores: ");
+        for (int i = 0; i< 10; i++)
+        {
+            Console.WriteLine("Posição: " + i + " Soma:  " + vetor3[i] );
+        }
+     
+    }
+}
