d4808beaec124f2d95532f6d30cb1eea1a5b0c22
Luiz Gustavo <luizgustavoms18@gmail.com>
2026-03-25 20:17:58 -0300

    commit

---
+++ AtividadeVetores07/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] vetor1 = new int[5];
+        int[] vetor2 = new int[5];
+
+        Console.WriteLine("Digite os valores do vetor:");
+
+        for (int i = 0; i < vetor1.Length; i++)
+        {
+            vetor1[i] = int.Parse(Console.ReadLine());
+        }
+
+        for (int i = 0; i < vetor1.Length; i++)
+        {
+            vetor2[i] = vetor1[vetor1.Length - 1 - i];
+        }
+
+
+        Console.WriteLine("Vetor invertido:");
+
+        for (int i = 0; i < vetor2.Length; i++)
+        {
+            Console.WriteLine(vetor2[i]);
+        }
+    }
+}
