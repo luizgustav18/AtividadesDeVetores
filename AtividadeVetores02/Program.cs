0d8c9d2a152b810e35d860e8a99458f9670c79eb
Luiz Gustavo <luizgustavoms18@gmail.com>
2026-03-25 19:40:04 -0300

    commit

---
+++ AtividadeVetores02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite valores para o vetor: ");
+        int[] vetor = new int[15];
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            vetor[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("\nNúmeros positivos dos vetores: ");
+        for (int i = 0; i < vetor.Length; i++)
+        {            
+            if (vetor[i] >= 0)
+            {
+                Console.WriteLine(vetor[i]);
+            }
+        }
+    }
+}
---
+++ AtividadesVetores01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.Serialization;
+class Program
+{
+    static void Main()
+    {
+        int soma = 0;
+
+        Console.Write("Digite valores para o vetor: ");
+        int[] vetor = new int[10];
+        
+
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            vetor[i] = int.Parse(Console.ReadLine());
+            soma = vetor[i] + soma;
+        }
+        Console.WriteLine("A soma do vetor: " + soma);
+    }
+}
